308584c240e3f6a9e67dc81685ed14472b901246
chonnigan <95559071+chonnigan@users.noreply.github.com>
2022-03-27 12:21:16 +0700

    Update home_page.dart

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -12,7 +12,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Quiz>? quiz_list;
+  List<Quiz>? quiz;
   int sum = 0;
   int wrong = 0;
   String message = "";
@@ -26,13 +26,13 @@ class _HomePageState extends State<HomePage> {
   void _fetch() async {
     List list = await Api().fetch('quizzes');
     setState(() {
-      quiz_list = list.map((item) => Quiz.fromJson(item)).toList();
+      quiz = list.map((item) => Quiz.fromJson(item)).toList();
     });
   }
 
   void guess(String choice) {
     setState(() {
-      if (quiz_list![sum].answer == choice) {
+      if (quiz![sum].answer == choice) {
         message = "เก่งมากค่ะคุณผู้เล่น";
       } else {
         message = "ตอบผิดนะคะ ตอบใหม่ค่ะ";
@@ -41,7 +41,7 @@ class _HomePageState extends State<HomePage> {
     Timer timer = Timer(Duration(seconds: 2), () {
       setState(() {
         message = "";
-        if (quiz_list![sum].answer == choice) {
+        if (quiz![sum].answer == choice) {
           sum++;
         } else {
           wrong++;
@@ -63,9 +63,9 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: quiz_list != null && sum < quiz_list!.length-1
+      body: quiz != null && sum < quiz!.length-1
           ? buildQuiz()
-          : quiz_list != null && sum == quiz_list!.length-1
+          : quiz != null && sum == quiz!.length-1
           ? buildTryAgain()
           : const Center(child: CircularProgressIndicator()),
     );
@@ -94,10 +94,10 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Image.network(quiz_list![sum].image_url, fit: BoxFit.cover),
+            Image.network(quiz![sum].image_url, fit: BoxFit.cover),
             Column(
               children: [
-                for (int i = 0; i < quiz_list![sum].choices.length; i++)
+                for (int i = 0; i < quiz![sum].choices.length; i++)
                   Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: Row(
@@ -105,8 +105,8 @@ class _HomePageState extends State<HomePage> {
                         Expanded(
                           child: ElevatedButton(
                             onPressed: () =>
-                                guess(quiz_list![sum].choices[i].toString()),
-                            child: Text(quiz_list![sum].choices[i]),
+                                guess(quiz![sum].choices[i].toString()),
+                            child: Text(quiz![sum].choices[i]),
                           ),
                         ),
                       ],
@@ -121,3 +121,4 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
